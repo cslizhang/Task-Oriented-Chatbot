9555ee20db1cdbaceb9e0fa418508fbc9d634947
evilpsycho <evilpsycho42@gmail.com>
2018-06-19 15:55:57 +0800

    update

--- chatbot/intent/attention/att_fasttext.py
+++ chatbot/intent/attention/att_fasttext.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
-# @Time    : 6/13/18 10:06
+# coding:utf8
+# @Time    : 18-6-19 上午11:05
 # @Author  : evilpsycho
 # @Mail    : evilpsycho42@gmail.com
 import torch
@@ -8,39 +8,34 @@ from torch.nn import functional as F
 from chatbot.intent.models.base_intent_model import BaseIntentModel
 
 
-class AttRCNN(BaseIntentModel):
-    def __init__(self, param, *args, **kwargs):
-        super().__init__(param, *args, **kwargs)
-        self.lookup = nn.Embedding(param["vocab_size"], param["embed_dim"])
-        self.conv11 = nn.Conv2d(1, 4, kernel_size=(3, param["embed_dim"]), padding=(1, 0))
-        self.conv12 = nn.Conv2d(1, 4, kernel_size=(5, param["embed_dim"]), padding=(2, 0))
-        self.rnn = nn.GRU(param["embed_dim"], param["hidden_dim"], batch_first=True)
-        self.fc = nn.Linear(param["hidden_dim"], param["class_num"])
+class AttFastText(BaseIntentModel):
+    def __init__(self, param: dict):
+        super().__init__(param)
+        embed_dim = param['embed_dim']
+        vocab_size = param['vocab_size']
+        class_num = param['class_num']
+        self.embed = nn.Embedding(vocab_size, embed_dim)
+        self.fc = nn.Linear(embed_dim, class_num)
+        self.score_w = nn.Parameter(torch.randn(param['embed_dim']))
+        self.score_b = nn.Parameter(torch.randn(1))
+
+    def _score(self, x, mask=None):
+        # inputs: embedded x
+        score = F.tanh(torch.matmul(x, self.score_w) + self.score_b)
+        if mask:
+            score.data.masked_fill_(mask, -float('inf'))
+        score = F.softmax(score, 1)
+        return score
 
     def forward(self, x):
-        embed = self.lookup(x)
-        att = self._attention(embed)
-        o, h = self.rnn(embed)
-        att = att.unsqueeze(2).expand_as(o)
-        att_o = att * o
-        att_o = torch.sum(att_o, dim=1, keepdim=False)
-        logit = F.log_softmax(self.fc(att_o), 1)
-        return logit
-
-    def _attention(self, x_embed):
-        x_embed = x_embed.unsqueeze(1)
-        a1 = F.relu(self.conv11(x_embed).squeeze(3))
-        a2 = F.relu(self.conv12(x_embed).squeeze(3))
-        # a1, a2 shape: (batch, kernel_num, seq_lenth)
-        a = torch.cat([a1, a2], 1)
-        a = torch.mean(a, dim=1, keepdim=False)
-        return F.softmax(a, 1)
-
-    def get_attention(self, x):
-        x_embed = self.lookup(x)
-        att = self._attention(x_embed)
-        return att
-
+        x = self.embed(x)
+        score = self._score(x)
+        print(score[0])
+        # x = torch.mean(x, dim=1, keepdim=False)
+        x_att = torch.sum(score.unsqueeze(2).expand_as(x) * x, dim=1, keepdim=False)
+        output = self.fc(x_att)
+        output = F.log_softmax(output, dim=1)
+        return output
 
 if __name__ == "__main__":
     import numpy as np
@@ -55,7 +50,7 @@ if __name__ == "__main__":
     test_x, test_y = x[7000:], y[7000:]
     import copy
     x = copy.deepcopy(train_x)
-    test_x, test_y = read_fasttext_file(str(p / "demo.train.txt"))
+    # test_x, test_y = read_fasttext_file(str(p / "demo.train.txt"))
     vocab = Vocabulary()
     vocab.fit(train_x)
     label = IntentLabel()
@@ -65,18 +60,17 @@ if __name__ == "__main__":
     train_y = np.array(label.transform(train_y))
     test_y = np.array(label.transform(test_y))
 
-    param = {
+    p = {
         "vocab_size": len(vocab),
         "embed_dim": 60,
         "class_num": len(label),
         "lr": 0.01,
-        "hidden_dim": 10,
         # "dropout": 0.5,
     }
-    model = AttRCNN(param)
+    model = AttFastText(p)
     model.fit(train_x, train_y, test_x, test_y, 2, 32, save_best=False)
-    model.param["lr"] = 0.003
-    model.fit(train_x, train_y, test_x, test_y, 4, 64, save_best=False)
+    # model.param["lr"] = 0.003
+    # model.fit(train_x, train_y, test_x, test_y, 4, 64, save_best=False)
     # model.save("test")
     # x = FastText.load(str(MODEL_PATH / "intent" / "test.FastText"))
     s = ["你真是可爱阿", "你很喜欢学习哦", "我再也不想理你了",
@@ -89,7 +83,7 @@ if __name__ == "__main__":
         print(i, label.reverse_one(model.infer(np.array(vocab.transform_one(cut(i), max_length=10)))[0]))
     from chatbot.evaluate.plot import plot_attention_1d
     idx=1200
-    att = model.get_attention(torch.tensor(np.array(vocab.transform_one(train_x[idx], max_length=10)).reshape(-1, 10)))
+    att = model._score(torch.tensor(np.array(vocab.transform_one(train_x[idx], max_length=10)).reshape(-1, 10)))
     print(label.reverse_one(model.infer(train_x[idx])[0]))
     plot_attention_1d([vocab.reverse_one(train_x[idx]).split(" ")],
                       att.detach().numpy())
--- docs/references/Attention.md
+++ docs/references/Attention.md
@@ -1,22 +1,10 @@
 # Attention
 
-对于序列建模，假设有句子矩阵$X=(x_1, x_2, … , x_n)$，其中$x_i$代表第$i$个词的词向量，维度为$d$，故$X \in R^{n\times d}$，对$X$的建模思路有下面3种：
-
-+ RNN： $y_i = f(y_{i-1}, x_i)$
-+ CNN：$y_i = f(x_{i-1}, x_{i}, x_{i+1})$
-+ Attention：$y_i = f(x_i, A, B)$
-
-首先给出Attention的定义，$Attention(Q,K,V)=softmax(\frac{nonlinear(Q,K)}{\sqrt[2]{d_k}})V$
-
-其中$Q\in R^{n\times d_k}$，代表Query，通常是decoder的隐藏状态，$K \in R^{m\times d_k}$，代表Key，通常是encoder的隐藏状态，$V \in R^{m \times d_v}$，代表与Key一一对应的标准化权重Value，代表着Key
-
-The [query] is usually the hidden state of the decoder. A [key] is the hidden state of the encoder and the corresponding [value] is the normalized weight, representing to what extent the key is being attended to. The way the query and the key are combined to get a value is by a weighted sum. Here, a dot product of the query and a key is taken to get a value.
-
 
 
 
 
 #### 引用
 
-[苏剑林[《Attention is All You Need》浅读（简介+代码）](https://kexue.fm/archives/4765)](https://kexue.fm/archives/4765)
-
+[苏剑林《Attention is All You Need》简介+代码](https://kexue.fm/archives/4765)](https://kexue.fm/archives/4765)
+[深入理解Attention机制](https://blog.csdn.net/yideqianfenzhiyi/article/details/79422857)
