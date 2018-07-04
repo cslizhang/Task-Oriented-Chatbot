79b432af1030c83af03c77b2fd7b7f7c01e94038
evilpsycho <evilpsycho42@gmail.com>
2018-07-04 17:25:15 +0800

    update

--- chatbot/utils/edit_distance.py
+++ chatbot/utils/edit_distance.py
@@ -5,7 +5,7 @@
 import numpy as np
 
 
-def substring_edit_distance(key_word, query):
+def _substring_edit_distance(key_word, query):
     """计算子串最小编辑距离函数，包含删除/添加/替换三种编辑操作
 
     Args:
@@ -17,13 +17,13 @@ def substring_edit_distance(key_word, query):
 
     Example:
 
-    >>> isinstance(substring_edit_distance("来福士", "sdf来福斯蒂芬"), int)
+    >>> isinstance(_substring_edit_distance("来福士", "sdf来福斯蒂芬"), int)
     True
 
-    >>> substring_edit_distance("abc", "c aba c")
+    >>> _substring_edit_distance("abc", "c aba c")
     1
 
-    >>> substring_edit_distance("来福士", "sdf福士斯蒂芬")
+    >>> _substring_edit_distance("来福士", "sdf福士斯蒂芬")
     1
 
     """
@@ -43,6 +43,53 @@ def substring_edit_distance(key_word, query):
     return int(min(matrix[-1, :]))
 
 
+class FuzzyMatch(object):
+    def __init__(self, keywords, threshold):
+        """
+
+        Args:
+            keywords:
+            threshold: int or list
+        """
+        if isinstance(threshold, int):
+            pass
+        elif isinstance(threshold, list):
+            assert len(threshold) == len(keywords)
+            assert isinstance(threshold[0], int)
+        else:
+            raise ValueError
+        self.threshold = threshold
+        self.keywords = keywords
+
+    def match(self, query):
+        """
+
+        Args:
+            query:
+
+        Returns:
+            list of string
+
+        """
+        rst = []
+        for idx, w in enumerate(self.keywords):
+            ed = _substring_edit_distance(w, query)
+            if isinstance(self.threshold, int):
+                if ed <= self.threshold:
+                    rst.append(w)
+                else:
+                    pass
+            else:
+                if ed <= self.threshold[idx]:
+                    rst.append(w)
+                else:
+                    pass
+        return rst
+
+
 if __name__ == "__main__":
     import doctest
     print(doctest.testmod())
+    m = FuzzyMatch(["来福士", "仁恒"], [1, 0])
+    m.match("哈哈来副士")
+    m.match("仁横用电")
